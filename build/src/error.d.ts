ed9cfd1e338b62690cf2790d8625eb06004f015f
Nate Silva <nate@natesilva.com>
2018-02-14 14:45:23 -0800

    remove build artifacts

--- build/src/error.d.ts
+++
@@ -1,2 +0,0 @@
-export declare class IpAddressError extends Error {
-}
--- build/src/error.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class IpAddressError extends Error {
-}
-exports.IpAddressError = IpAddressError;
-//# sourceMappingURL=error.js.map
